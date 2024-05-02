e625f6a8087012b31adb6a0bfa7c31721aff0910
dev1as <168581503+dev1as@users.noreply.github.com>
2024-05-02 20:19:36 +0200

    Add files via upload

---
+++ data/data.mjs
@@ -0,0 +1,30 @@
+const data = [
+    {
+        id: 1,
+        imgLocker: `../imgs/locker1.webp`,
+        title: `offer 1`,
+        description: `
+           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        `,
+        device: `All`,
+        category: `Emait & Submit`, 
+        date: `3 April, 2022`,
+        link_locker: `https://installyourfiles.com/1680921`,
+    },
+
+    {
+        id: 2,
+        imgLocker: `../imgs/locker2.jpg`,
+        title: `offer 2`,
+        description: `
+           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        `,
+        device: `All`,
+        category: `Emait & Submit`, 
+        date: `3 April, 2022`,
+        link_locker: `https://installyourfiles.com/1680921`,
+    },
+
+];
+
+export default data;
