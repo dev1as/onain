0888663b0c20b6eb0204511b3b127845617425e1
dev1as <168581503+dev1as@users.noreply.github.com>
2024-05-02 20:21:33 +0200

    data.mjs

--- data/data.mjs
+++ data/data.mjs
@@ -1,7 +1,7 @@
 const data = [
     {
         id: 1,
-        imgLocker: `../imgs/locker1.webp`,
+        imgLocker: `./imgs/locker1.webp`,
         title: `offer 1`,
         description: `
            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
@@ -14,8 +14,44 @@ const data = [
 
     {
         id: 2,
-        imgLocker: `../imgs/locker2.jpg`,
+        imgLocker: `./imgs/locker2.jpg`,
         title: `offer 2`,
+        description: `
+           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        `,
+        device: `All`,
+        category: `Emait & Submit`, 
+        date: `3 April, 2022`,
+        link_locker: `https://installyourfiles.com/1680921`,
+    },
+{
+        id: 3,
+        imgLocker: `./imgs/locker2.jpg`,
+        title: `offer 3`,
+        description: `
+           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        `,
+        device: `All`,
+        category: `Emait & Submit`, 
+        date: `3 April, 2022`,
+        link_locker: `https://installyourfiles.com/1680921`,
+    },
+{
+        id: 4,
+        imgLocker: `./imgs/locker2.jpg`,
+        title: `offer 4`,
+        description: `
+           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        `,
+        device: `All`,
+        category: `Emait & Submit`, 
+        date: `3 April, 2022`,
+        link_locker: `https://installyourfiles.com/1680921`,
+    },
+{
+        id: 5,
+        imgLocker: `./imgs/locker2.jpg`,
+        title: `offer 5`,
         description: `
            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         `,
